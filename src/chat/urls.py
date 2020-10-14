e1ba3c6bf4c2208e5afad8f292c55a495d791c2e
mustafa <mustafagamaldeveloper@gmail.com>
2020-10-14 15:15:44 +0200

    Create LogoutView

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('signin/', views.SigninView.as_view(), name="signin"),
+    path('logout/', views.LogoutView.as_view(), name="logout"),
     path('signup/', views.signup, name="signup"),
 ]
--- src/chat/views/signin.py
+++ src/chat/views/signin.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render, reverse
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView
+from django.shortcuts import reverse
+import django.contrib.auth.views as views
 from django.contrib import messages
 
 
-class SigninView(LoginView):
+class SigninView(views.LoginView):
     template_name = "chat/sign-in.html"
     
     def get_success_url(self):
         return reverse("index")
+
+
+class LogoutView(views.LogoutView):
+    
+    def get_success_url(self):
+        return reverse("signin")
